98ce43b7c9b79fcf067a5eee9aeaa9180bc45965
BaeZzi813 <woehd8252@gmail.com>
2026-01-06 16:23:58 +0900

    appspec / deploy 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/ec2-test 
-git pull origin main
+cd /home/ubuntu/ec2-test
 sudo npm install
 sudo npm run build
-pm2 restart next_app
+pm2 delete next_app || true
+pm2 start npm --name "next_app" -- start
