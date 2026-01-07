5354e5b76f77ec59e9a8786f797eca8c1ab67700
BaeZzi813 <woehd8252@gmail.com>
2026-01-07 17:39:00 +0900

    deploy 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,19 @@ DEPLOY_DIR=/home/ubuntu/EC2-Deploy-Test
 REPO_URL=https://github.com/BaeZzi813/EC2-Deploy-Test.git
 BRANCH=main
 
-# 기존 폴더가 없으면 clone, 있으면 최신 커밋으로 reset
-if [ ! -d "$DEPLOY_DIR/.git" ]; then
-    git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
-else
-    cd $DEPLOY_DIR
-    git fetch --all
-    git reset --hard origin/$BRANCH
+# 기존 폴더가 있으면 삭제 후 새로 clone
+if [ -d "$DEPLOY_DIR" ]; then
+    rm -rf "$DEPLOY_DIR"
 fi
+
+git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
 
 # 권한 정리
 sudo chown -R ubuntu:ubuntu $DEPLOY_DIR
 
-# 패키지 설치 및 빌드
+# 패키지 설치 & 빌드
 npm install
 npm run build
 
